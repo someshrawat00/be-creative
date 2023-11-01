8df29d881290cfb7de6eb15c020a2257f52d72e8
someshrawat00 <someshrawat00@gmail.com>
2023-11-01 11:05:56 +0530

    d

--- app/components/Hero/animations.js
+++ app/components/Hero/animations.js
@@ -10,7 +10,7 @@ export const pageAnimations = (pageRef) =>{
         ease: "expo.inOut",
     }).from('[data-title-item]',{
         y: 120,
-        duration: 2.5,
+        duration: 1.5,
         stagger:0.05,
         ease: "sine.inOut",
     })
